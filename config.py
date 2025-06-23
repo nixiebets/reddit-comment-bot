ffc1f45bf3c93df04b200902f9427d8133d87dc7
nixiebets <nixiemgmt@gmail.com>
2025-06-23 11:50:31 -0700

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,60 @@ REDDIT_CLIENT_ID = os.environ["REDDIT_CLIENT_ID"]
 REDDIT_CLIENT_SECRET = os.environ["REDDIT_SECRET"]
 REDDIT_USER_AGENT = os.environ["REDDIT_USER_AGENT"]
 
-TARGET_SUBREDDIT = "Test_Posts"  # or whichever you're testing
+# config.py
+TARGET_SUBREDDITS = ["ChatGPT",
+   "ArtificialIntelligence",
+    "PromptEngineering",
+    "Automation",
+    "Notion",
+    "Productivity",
+    "ObsidianMD",
+    "SideProject",
+    "Entrepreneur",
+    "SideHustle",
+    "Gumroad",
+    "Passive_Income",
+    "SaaS",
+    "InternetIsBeautiful",
+    "Copywriting",
+    "ContentCreators",
+    "SocialMedia",
+    "NoCode",
+    "TechSupport",
+    "Freelance",
+    "SmallBusiness",
+    "LifeProTips",
+    "GetDisciplined",
+    "SelfImprovement",
+    "Workspaces"]
+
+# reddit_bot.py
+subreddit = reddit.subreddit("+".join("ChatGPT",
+    "ArtificialIntelligence",
+    "PromptEngineering",
+    "Automation",
+    "Notion",
+    "Productivity",
+    "ObsidianMD",
+    "SideProject",
+    "Entrepreneur",
+    "SideHustle",
+    "Gumroad",
+    "Passive_Income",
+    "SaaS",
+    "InternetIsBeautiful",
+    "Copywriting",
+    "ContentCreators",
+    "SocialMedia",
+    "NoCode",
+    "TechSupport",
+    "Freelance",
+    "SmallBusiness",
+    "LifeProTips",
+    "GetDisciplined",
+    "SelfImprovement",
+    "Workspaces"))
+
 TARGET_STRING = "workflow"
 REPLY_MESSAGE = "here's a solid resource for automating tasks with AI: https://cutt.ly/promptkitmini"
 SLEEP_DURATION = 10
