95062f794cdd1f72f00d604eef3a0ad6c81920d4
nixiebets <nixiemgmt@gmail.com>
2025-06-22 18:41:48 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 # Reddit account credentials
-REDDIT_USERNAME = "YourRedditUsername"
-REDDIT_PASSWORD = "YourRedditPassword"
+REDDIT_USERNAME = "journal_fiend"
+REDDIT_PASSWORD = "thisisapassword1234"
 
 # Reddit API credentials
-REDDIT_CLIENT_ID = "YourClientID"
-REDDIT_CLIENT_SECRET = "YourClientSecret"
+REDDIT_CLIENT_ID = "wgKIu40KpFoFWNUDYfpXQ"
+REDDIT_CLIENT_SECRET = "jh08uSTF1a8R8LsJskztjHGLNGooVg"
 
 # Bot configuration
-REDDIT_USER_AGENT = "YourUniqueUserAgent v1.0"
-TARGET_SUBREDDIT = "test"  # Replace with your desired subreddit
+REDDIT_USER_AGENT = "promptkit-bot by u/journal_fiend"
+TARGET_SUBREDDIT = "ChatGPT"  # Replace with your desired subreddit
 TARGET_STRING = "sample user comment"  # Replace with your desired search string
-REPLY_MESSAGE = "Hey, I like your comment!"  # Replace with your desired reply message
+REPLY_MESSAGE = "Solid resource for automating tasks with AI if you’re stuck in decision fatigue. Free version here: https://cutt.ly/promptkitmini"  # Replace with your desired reply message
 
 # Bot behavior configuration
 SLEEP_DURATION = 10  # Adjust the sleep duration as needed in seconds
